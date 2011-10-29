4772d65bdb67d95d1afb8424f94cb14a3adb7e26
Devon Jones <devon.jones@gmail.com>
2011-10-29 15:52:11 -0400

    added rules parsing, works over 5 files so far

---
+++ src/local_rules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+./rules_parse.py -o ../local/ -b "Core Rulebook" -t "Getting Started" ../../../data/websites/Pathfinder\ psrd/paizo.com/pathfinderRPG/prd/gettingStarted.html
+./rules_parse.py -o ../local/ -b "Core Rulebook" -t "Classes" ../../../data/websites/Pathfinder\ psrd/paizo.com/pathfinderRPG/prd/classes.html
+./rules_parse.py -o ../local/ -b "Core Rulebook" -t "Using Skills" ../../../data/websites/Pathfinder\ psrd/paizo.com/pathfinderRPG/prd/usingSkills.html
+./rules_parse.py -o ../local/ -b "Core Rulebook" -t "Skill Descriptions" ../../../data/websites/Pathfinder\ psrd/paizo.com/pathfinderRPG/prd/skillDescriptions.html
+./rules_parse.py -o ../local/ -b "Core Rulebook" -t "Equipment" ../../../data/websites/Pathfinder\ psrd/paizo.com/pathfinderRPG/prd/equipment.html
+./rules_parse.py -o ../local/ -b "Core Rulebook" -t "Additional Rules" ../../../data/websites/Pathfinder\ psrd/paizo.com/pathfinderRPG/prd/additionalRules.html
+./rules_parse.py -o ../local/ -b "Core Rulebook" -t "Combat" ../../../data/websites/Pathfinder\ psrd/paizo.com/pathfinderRPG/prd/combat.html
+./rules_parse.py -o ../local/ -b "Core Rulebook" -t "Magic" ../../../data/websites/Pathfinder\ psrd/paizo.com/pathfinderRPG/prd/magic.html
+
---
+++ src/rules_parse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+import os
+import re
+import json
+from psrd.rules import parse_rules
+from psrd.options import exec_main, option_parser
+
+def main():
+	usage = "usage: %prog [options] [filenames]\nParses rules from psrd html to json and writes them to the specified directory"
+	parser = option_parser(usage, title=True)
+	exec_main(parser, parse_rules, "rules")
+
+if __name__ == "__main__":
+	sys.exit(main())
